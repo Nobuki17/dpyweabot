6972e25e3cde2dc62af69013b2761153f6524fe3
WeaponJP <49573018+Nobuki17@users.noreply.github.com>
2020-06-17 18:18:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,7 +8,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 @bot.event
 async def on_command_error(ctx, error):
-    await bot.change_presence(activity=discord.Game(f"ヘルプは wb:help | 導入サーバー数: {len(client.guilds)}"))
+    await client.change_presence(activity=discord.Game(f"ヘルプは wb:help | 導入サーバー数: {len(client.guilds)}"))
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
