039b4a06c0a4304081f26e594d9053217f64d533
WeaponJP <49573018+Nobuki17@users.noreply.github.com>
2020-07-06 19:14:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -45,7 +45,7 @@ async def about(ctx):
     embed = discord.Embed(title="このbotについて...", description="Weabot / うぇあぼっと",color=0x77aa27)
     embed.add_field(name="製作者", value="うぇあChannel#6928",inline=True)
     embed.add_field(name="バージョン", value="Ver.1.6a\nコマンドフレームワーク移行版",inline=False)
-    embed.add_field(name="招待リンク", value="https://discord.com/oauth2/authorize?client_id=699585993988374628&permissions=379968&scope=bot",inline=False)
+    embed.add_field(name="このbotを招待", value="[こちら](https://discord.com/oauth2/authorize?client_id=699585993988374628&permissions=379968&scope=bot)から招待できます",inline=False)
     await ctx.send(embed=embed)
 
 
