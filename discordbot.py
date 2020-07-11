06a440f7374b981cf1d65aaa753626d887b31ef4
WeaponJP <49573018+Nobuki17@users.noreply.github.com>
2020-07-11 17:04:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,7 +9,6 @@ import traceback
 
 bot = commands.Bot(command_prefix="wb:", help_command=None)
 token = os.environ['DISCORD_BOT_TOKEN']
-GLOBAL_CH_NAME = "weabot-global" # グローバルチャットのチャンネル名
 
 
 @bot.event
@@ -148,28 +147,6 @@ async def on_message(message):
         
     elif message.content.startswith("初めまして"):
         await message.channel.send("初めまして！")
-
-    elif message.channel.name == GLOBAL_CH_NAME:
-        await message.delete() # 元のメッセージは削除しておく
-        # hoge-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
-
-        channels = client.get_all_channels()
-        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
-        # channelsはbotの取得できるチャンネルのイテレーター
-        # global_channelsは hoge-global の名前を持つチャンネルのリスト
-        
-        embed = discord.Embed(title="weabot-global",
-            description=message.content, color=0x00bfff)
-
-        embed.set_author(name=message.author.display_name, 
-            icon_url=message.author.avatar_url_as(format="png"))
-        embed.set_footer(text=f"{message.guild.name} / {message.channel.name}",
-            icon_url=message.guild.icon_url_as(format="png"))
-        # Embedインスタンスを生成、投稿者、投稿場所などの設定
-
-        for channel in global_channels:
-            # メッセージを埋め込み形式で転送
-            await channel.send(embed=embed)
 
     await bot.process_commands(message)#on_messageの定義内の最後にこれを入れないと定義したコマンドが動かなくなる。注意
 
