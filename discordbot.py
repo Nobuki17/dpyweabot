8bbcc1fa5aec2cc53673d86237a3da1caf20bbab
WeaponJP <49573018+Nobuki17@users.noreply.github.com>
2020-07-11 16:55:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -146,7 +146,29 @@ async def on_message(message):
         await message.channel.send("暇ですねぇ...")
         
     elif message.content.startswith("初めまして"):
-        await message.channel.send("初めまして！")                                  
+        await message.channel.send("初めまして！")
+        
+        GLOBAL_CH_NAME = "hoge-global" # グローバルチャットのチャンネル名
+
+    elif message.channel.name == weabot-global:
+        # hoge-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
+
+        await message.delete() # 元のメッセージは削除しておく
+
+        channels = client.get_all_channels()
+        global_channels = [ch for ch in channels if ch.name == weabot-global]
+        # channelsはbotの取得できるチャンネルのイテレーター
+        # global_channelsは hoge-global の名前を持つチャンネルのリスト
+
+        embed.set_author(name=message.author.display_name, 
+            icon_url=message.author.avatar_url_as(format="png"))
+        embed.set_footer(text=f"{message.guild.name} / {message.channel.name}",
+            icon_url=message.guild.icon_url_as(format="png"))
+        # Embedインスタンスを生成、投稿者、投稿場所などの設定
+
+        for channel in global_channels:
+            # メッセージを埋め込み形式で転送
+            await channel.send(embed=embed)
 
     await bot.process_commands(message)#on_messageの定義内の最後にこれを入れないと定義したコマンドが動かなくなる。注意
 
